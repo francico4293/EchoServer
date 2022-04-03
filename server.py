f9c811628bec0c5be45487b0c3ebeaefb7319d8e
francico4293 <francico@oregonstate.edu>
2022-04-03 03:11:58 -0400

    Added code for creating listening socket

--- server.py
+++ server.py
@@ -2,12 +2,39 @@
 # Description: An echo server used to broadcast messages to connected clients
 
 from socket import *
-from os import *
+import os
+from sys import *
 
 
 class EchoServer(object):
-    pass
+    """Constructor for EchoServer class object"""
+    def __init__(self, serverAddr: str, serverPort: int) -> None:
+        self._serverAddr = serverAddr
+        self._serverPort = serverPort
+        self._backlog = 10
+        self._clientPool = []
 
+    def _handleSIGCLD(self) -> None:
+        pass
+
+    """"""
+    def _createListenSocket(self) -> socket:
+        # create TCP socket
+        listenSocket = socket(AF_INET, SOCK_STREAM)
+        # bind socket to specified address at specified port
+        listenSocket.bind((self._serverAddr, self._serverPort))
+        # mark socket as a listening socket with specified backlog
+        listenSocket.listen(self._backlog)
+        # display "server running" message to user
+        print("[+] Server running on port {}".format(self._serverPort))
+        # return listen socket back to calling function
+        return listenSocket
+    
+    """Starts the server such that it begins listening for client connections"""
+    def start(self) -> None:
+        listenSocket = self._createListenSocket()
+        
 
 if __name__ == "__main__":
-    pass
+    s = EchoServer("127.0.0.1", 59028)
+    s.start()
